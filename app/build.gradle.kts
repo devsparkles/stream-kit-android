7880e9b24d0458150c4190412ba2a239a73b735f
Xavier Maximin <xmaximin@gmail.com>
2026-03-23 16:53:35 +0100

    added exoplayer media3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,10 @@ dependencies {
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
 
+    // exoplayer media3
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.exoplayer.dash)
+    implementation(libs.androidx.media3.ui)
+    implementation(libs.androidx.media3.ui.compose)
+
 }
